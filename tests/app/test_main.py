d9fb6c46e87fa6ba788a0d576ad986be0597dfd2
symonk <jackofspaces@gmail.com>
2025-08-05 22:13:31 +0100

    improving `/health` endpoint tests, fix `pre-commit`

--- tests/app/test_main.py
+++ tests/app/test_main.py
@@ -5,10 +5,12 @@ from app import main
 import json
 import typing
 
+
 def test_analyse_returns_200_ok() -> typing.NoReturn:
     client = TestClient(main.app)
     response = client.get("/health")
     assert_that(response.status_code == HTTPStatus.OK)
-    body = json.loads(response.content)
-    assert_that(body).contains_key("uptime")
+    assert_that(body := json.loads(response.content)).is_type_of(dict).contains_key(
+        "uptime"
+    )
     assert_that(body["uptime"]).matches(r"\d+ seconds")
